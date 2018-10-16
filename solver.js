d6502998a5be5440b10279d51047fcd55a600de4
georgebabayan <ggbabayan@gmail.com>
2018-10-16 16:23:04 +0300

    Aads jasmine tests

--- solver.js
+++ solver.js
@@ -1,5 +1,5 @@
 // This method reads a file into a 2D array. See `spec/solver_spec.rb`
-// Можете обратиться к вашему решению судоку - 
+// Можете обратиться к вашему решению судоку -
 // там была схожая работа с файловой системой.
 function readMaze(file) {
 
--- spec/solver_spec.js
+++ spec/solver_spec.js
@@ -1,10 +1,10 @@
-solver = require('../solver')
+solver = require('../solver.js')
 
 describe("Solver", () => {
   describe('read_maze', () => {
     it('should read a maze into a 2D (nested) array', () => {
-      maze = readMaze("./map_test.txt")
-      maze_array = [['#','.','#'],
+      let maze = readMaze("./map_test.txt")
+      let mazeArray = [['#','.','#'],
                     ['#','.','*'],
                     ['#','#','#']]
       expect(maze).toEqual(mazeArray)
