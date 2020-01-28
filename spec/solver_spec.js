7f0d0075b8f241d6524220d8e9563bb50eec50a9
Alekskupr <leshakuprish@yandex.ru>
2020-01-28 14:30:24 +0300

    jasmine in console v2.0

--- spec/solver_spec.js
+++ spec/solver_spec.js
@@ -1,14 +1,16 @@
-solver = require('../solver.js'); 
-const readMaze = require('../solver');
+solver = require('../solver.js');
+const { readMaze } = require('../solver');
 
-describe("Solver", () => {
+describe('Solver', () => {
   describe('read_maze', () => {
     it('should read a maze into a 2D (nested) array', () => {
-      let maze = readMaze("./map_test.txt")
-      let mazeArray = [['#','.','#'],
-                    ['#','.','*'],
-                    ['#','#','#']]
-      expect(maze).toEqual(mazeArray)
-    })
-  })
-})
+      let maze = readMaze('./map_test.txt');
+      let mazeArray = [
+        ['#', '.', '#'],
+        ['#', '.', '*'],
+        ['#', '#', '#'],
+      ];
+      expect(maze).toEqual(mazeArray);
+    });
+  });
+});
